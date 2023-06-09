cf787e380721a73741716b0ebd5924a8b1feb2f7
SofiaCurreri <sofiacurreri@gmail.com>
2023-06-09 14:44:11 +0200

    created and printed an ArrayList called wishList

---
+++ src/org/lessons/java/christmas/WishList.java
@@ -0,0 +1,33 @@
+package org.lessons.java.christmas;
+
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
+public class WishList {
+    public static void main(String[] args) {
+        Scanner utente = new Scanner(System.in);
+
+        ArrayList<String> wishList = new ArrayList<>();
+        boolean exit = false;
+
+        do{
+            System.out.println("Vuoi inserire un regalo nella tua wish list? (s/n)");
+            String choice = utente.nextLine();
+            if(choice.equals("s")){
+                System.out.println("Inserisci un regalo");
+                String present = utente.nextLine();
+                wishList.add(present);
+            } else if (choice.equals("n")) {
+                exit = true;
+                System.out.println("Sei uscito dalla tua wish list");
+            } else {
+                System.out.println("Devi inserire 's' o 'n', che equivalgono a 'si' o 'no'");
+            }
+        }while(!exit);
+
+        System.out.println("\nEcco la tua wish list: ");
+        for (String present : wishList) {
+            System.out.println(present);
+        }
+    }
+}
